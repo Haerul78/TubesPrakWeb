207894135311ea286705ee56398e297885a97c21
Haerul78 <haerulrn20@gmail.com>
2024-05-23 17:39:29 +0700

    tambah

---
+++ file/test.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint necessitatibus ex adipisci natus, repudiandae in odio ut amet quaerat? Soluta, assumenda. At eaque corrupti possimus. Impedit eum sunt doloremque nemo.</p>
